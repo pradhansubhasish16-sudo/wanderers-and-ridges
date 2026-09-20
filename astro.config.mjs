6a73405ed160d52b12f54c46775837be9c3ee037
Subhasish Pradhan <pradhansubhasish16@gmail.com>
2026-09-21 00:57:00 +0530

    ci: fix github pages actions deploy and disable jekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://pradhansubhasish16-sudo.github.io',
+  base: '/wanderers-and-ridges',
   integrations: [
     tailwind({
       applyBaseStyles: false,
