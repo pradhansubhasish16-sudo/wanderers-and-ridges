e0c388556bb7fe1f9fee8ba4447a2c769b7c3fe6
Subhasish Pradhan <pradhansubhasish16@gmail.com>
2026-09-21 01:00:30 +0530

    Revert "ci: fix github pages actions deploy and disable jekyll"
    This reverts commit 6a73405ed160d52b12f54c46775837be9c3ee037.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://pradhansubhasish16-sudo.github.io',
-  base: '/wanderers-and-ridges',
   integrations: [
     tailwind({
       applyBaseStyles: false,
